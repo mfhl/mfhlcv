024c4e7fce0ea798a41b4529e1c81083344cc714
mfhl <fer9210-10@hotmail.com>
2022-10-13 18:34:46 -0500

    commit
    pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//pages
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SkillComponent } from './pages/skill/skill.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SkillComponent } from './pages/skill/skill.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    AboutComponent,
+    NotFoundComponent,
+    SkillComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
